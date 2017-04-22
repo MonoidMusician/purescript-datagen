580846e44afb0b0619182b8a310fb9d5c04455ab
Nicholas Scheel <nasalmusician@gmail.com>
2017-04-22 18:15:41 -0500

    Add TextArea Halogen Lens component

--- src/Component.purs
+++ src/Component.purs
@@ -1,20 +1,21 @@
 module Component where
 
 import Prelude
+import Data.Array as Array
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
-import Halogen.HTML.Properties as HP
 import Halogen.HTML.Lens as HL
 import Halogen.HTML.Lens.Input as HL.Input
+import Halogen.HTML.Lens.TextArea as HL.TextArea
+import Halogen.HTML.Properties as HP
 import Control.Monad.Aff (Aff)
 import DOM (DOM)
 import Data.Array (filter, foldr, intercalate)
-import Data.Array as Array
 import Data.Char (toUpper)
-import Data.Maybe (Maybe(..))
 import Data.Lens.Suggestion (Lens', lens, suggest)
-import Data.String (joinWith, singleton, uncons)
+import Data.Maybe (Maybe(..))
+import Data.String (Pattern(..), joinWith, singleton, split, uncons)
 import Data.String.Utils (words)
 
 type Query = HL.Query State
@@ -24,13 +25,17 @@ type Element p = H.HTML p Query
 type State =
   { description :: String
   , name :: String
+  , executeBody :: String
   }
 
 descriptionL :: Lens' State String
 descriptionL = lens (_.description) (\s d -> s { description = d })
 
 nameL :: Lens' State String
-nameL = lens (_.name) (\s d -> s { name = d })
+nameL = lens (_.name) (\s n -> s { name = n })
+
+executeL :: Lens' State String
+executeL = lens (_.executeBody) (\s e -> s { executeBody = e })
 
 toName :: String -> String
 toName = words >>> exclude >>> map camel >>> joinWith ""
@@ -53,6 +58,9 @@ descriptionComponent = HL.Input.renderAsField "Description" suggestDescriptionL
 nameComponent :: forall p. State -> Element p
 nameComponent = HL.Input.renderAsField "Name" nameL
 
+executeComponent :: forall p. State -> Element p
+executeComponent = HL.TextArea.renderAsField "Execute" executeL
+
 component :: forall eff. H.Component HH.HTML Query Unit Void (Aff (dom :: DOM | eff))
 component =
   H.component
@@ -67,6 +75,7 @@ component =
   initialState =
     { description: "Aim the camera"
     , name: "AimCamera"
+    , executeBody: "Robot::camera->Set(0);"
     }
 
   render :: State -> H.ComponentHTML Query
@@ -76,6 +85,7 @@ component =
           [ HH.text "Create FRC Command" ]
       , descriptionComponent state
       , nameComponent state
+      , executeComponent state
       , HH.a_ $ texts ["Commands/", name, ".h" ]
       , generateHeader state
       , HH.a_ $ texts ["Commands/", name, ".cpp" ]
@@ -112,7 +122,7 @@ generateSource state@{ description, name } = HH.pre_ $
     [ commentline description
     , method name [] []
     , method "Initialize" [] []
-    , method "Execute" [] []
+    , method "Execute" [] (texts $ split (Pattern "\n") state.executeBody)
     , method "IsFinished" [] []
     , method "Done" [] []
     , method "Interrupted" [] []
@@ -126,7 +136,7 @@ generateSource state@{ description, name } = HH.pre_ $
             , "("
             ]
         , separate (HH.text ", ") args
-        , from [ ") {\n" ]
+        , from [ ") {\n\t" ]
         , separate (HH.text "\n\t") body
-        , from [ "}\n\n" ]
+        , from [ "\n}\n\n" ]
         ]
---
+++ src/lib/Halogen/HTML/Lens/TextArea.purs
@@ -0,0 +1,53 @@
+module Halogen.HTML.Lens.TextArea
+    ( setter, query, attr,
+    render, renderAsField
+    ) where
+
+import Prelude
+import Halogen.HTML.Lens (Query(..))
+import DOM.Event.Event as Event
+import DOM.HTML.HTMLTextAreaElement as HTextArea
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Control.Monad.Eff (Eff)
+import Control.Monad.Except (runExcept)
+import DOM (DOM)
+import DOM.Event.Types (Event)
+import DOM.HTML.Types (readHTMLTextAreaElement)
+import Data.Either (Either(..))
+import Data.Foreign (toForeign)
+import Data.Lens ((.~), (^.))
+import Data.Lens.Types (Lens')
+
+type Property s p = H.IProp p (Query s)
+type Element s p = H.HTML p (Query s)
+
+setter :: forall s eff. Lens' s String -> Event -> Eff (dom :: DOM | eff) (s -> s)
+setter lens e =
+    case runExcept $ readHTMLTextAreaElement $ toForeign $ Event.target e of
+        Left _ -> pure id
+        Right node -> do
+            value <- H.liftEff $ HTextArea.value node
+            pure (lens .~ value)
+
+query :: forall s a. Lens' s String -> Event -> a -> Query s a
+query lens e = UpdateState (setter lens e)
+
+attr :: forall s p. Lens' s String -> Property s (onInput :: Event | p)
+attr lens = HE.onInput (HE.input (query lens))
+
+render :: forall s p. Lens' s String -> s -> Element s p
+render lens state = HH.textarea
+    [ attr lens
+    , HP.value (state ^. lens)
+    ]
+
+renderAsField :: forall s p. String -> Lens' s String -> s -> Element s p
+renderAsField label lens state =
+    HH.div_
+        [ HH.text (label <> ": ")
+        , render lens state
+        ]
+    where value = state ^. lens
