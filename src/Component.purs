6a91f525e92b27276ce14be34551a3ced059d1c3
Nicholas Scheel <monoidmusician@gmail.com>
2017-08-13 07:54:15 -0500

    Pirate frc-codegen project, start converting

--- src/Component.purs
+++ src/Component.purs
@@ -1,27 +1,25 @@
 module Component where
 
 import Prelude
-import Data.Array as Array
-import Halogen as H
-import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
-import Halogen.HTML.Lens as HL
-import Halogen.HTML.Lens.Input as HL.Input
-import Halogen.HTML.Lens.TextArea as HL.TextArea
-import Halogen.HTML.Lens.Checkbox as HL.Checkbox
-import Halogen.HTML.Properties as HP
+
 import Control.Monad.Aff (Aff)
 import DOM (DOM)
 import Data.Array (filter, foldr, intercalate)
+import Data.Array as Array
 import Data.Char (toUpper)
 import Data.Lens.Suggestion (Lens', lens, suggest)
 import Data.Maybe (Maybe(..))
 import Data.String (Pattern(..), joinWith, singleton, split, uncons)
-import Data.String (toUpper) as Str
+import Data.String (toUpper, toLower) as Str
 import Data.String.Utils (words)
-import Data.String.Regex (split) as Re
-import Data.String.Regex.Unsafe (unsafeRegex) as Re
-import Data.String.Regex.Flags (global) as Re
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Lens as HL
+import Halogen.HTML.Lens.Checkbox as HL.Checkbox
+import Halogen.HTML.Lens.Input as HL.Input
+import Halogen.HTML.Lens.TextArea as HL.TextArea
+import Halogen.HTML.Properties as HP
 
 type Query = HL.Query State
 
@@ -30,8 +28,8 @@ type Element p = H.HTML p Query
 type State =
   { description :: String
   , name :: String
-  , executeBody :: String
-  , isInstant :: Boolean
+  , fieldPrefix :: String
+  , hasSourceAnn :: Boolean
   }
 
 descriptionL :: Lens' State String
@@ -40,18 +38,18 @@ descriptionL = lens (_.description) (\s d -> s { description = d })
 nameL :: Lens' State String
 nameL = lens (_.name) (\s n -> s { name = n })
 
-executeL :: Lens' State String
-executeL = lens (_.executeBody) (\s e -> s { executeBody = e })
+fieldPrefixL :: Lens' State String
+fieldPrefixL = lens (_.fieldPrefix) (\s e -> s { fieldPrefix = e })
 
-isInstantL :: Lens' State Boolean
-isInstantL = lens (_.isInstant) (\s i -> s { isInstant = i })
+hasSourceAnnL :: Lens' State Boolean
+hasSourceAnnL = lens (_.hasSourceAnn) (\s i -> s { hasSourceAnn = i })
 
 toName :: String -> String
 toName = words >>> exclude >>> map camel >>> joinWith ""
     where
     blacklist = ["this", "the","a","an","it"]
     exclude ws =
-        foldr (\w -> filter (w /= _)) ws blacklist
+        foldr (\w -> filter (not eq w <<< Str.toLower)) ws blacklist
     camel s =
         case uncons s of
             Nothing -> s
@@ -67,11 +65,11 @@ descriptionComponent = HL.Input.renderAsField "Description" suggestDescriptionL
 nameComponent :: forall p. State -> Element p
 nameComponent = HL.Input.renderAsField "Name" nameL
 
-executeComponent :: forall p. State -> Element p
-executeComponent = HL.TextArea.renderAsField "Execute" executeL
+fieldPrefixComponent :: forall p. State -> Element p
+fieldPrefixComponent = HL.Input.renderAsField "Prefix for the fields" fieldPrefixL
 
-instantComponent :: forall p. State -> Element p
-instantComponent = HL.Checkbox.renderAsField "Instant command" isInstantL
+hasSourceAnnComponent :: forall p. State -> Element p
+hasSourceAnnComponent = HL.Checkbox.renderAsField "Contains a source annotation" hasSourceAnnL
 
 component :: forall eff. H.Component HH.HTML Query Unit Void (Aff (dom :: DOM | eff))
 component =
@@ -85,24 +83,21 @@ component =
 
   initialState :: State
   initialState =
-    { description: "Aim the camera"
-    , name: "AimCamera"
-    , executeBody: "Robot::camera->Set(0);"
-    , isInstant: false
+    { description: "A type declaration"
+    , name: "TypeDeclaration"
+    , fieldPrefix: "tydecl"
+    , hasSourceAnn: true
     }
 
   render :: State -> H.ComponentHTML Query
   render state@{ description, name } =
     HH.div_
       [ HH.h1_
-          [ HH.text "Create FRC Command" ]
+          [ HH.text "Create PureScript Compiler Data Type" ]
       , descriptionComponent state
       , nameComponent state
-      , executeComponent state
-      , instantComponent state
-      , HH.a_ $ texts ["Commands/", name, ".h" ]
-      , generateHeader state
-      , HH.a_ $ texts ["Commands/", name, ".cpp" ]
+      , fieldPrefixComponent state
+      , hasSourceAnnComponent state
       , generateSource state
       ]
 
@@ -116,51 +111,19 @@ from :: forall p. Array String -> Element p
 from = HH.span_ <<< texts
 
 commentline :: forall p. String -> Element p
-commentline text = from [ "// ", text, "\n" ]
+commentline text = from [ "-- ", text, "\n" ]
 
 separate :: forall p i. HH.HTML p i -> Array (HH.HTML p i) -> HH.HTML p i
 separate sep = HH.span_ <<< intercalate [sep] <<< map Array.singleton
 
-generateHeader :: forall p. State -> Element p
-generateHeader state@{ description, name, isInstant } = HH.pre_ $
-    [ from [ "#ifndef ", macroname, "\n", "#define ", macroname, "\n\n" ]
-    , commentline description
-    , from [ "class ", name, " : public frc::", baseclass, " {\n"]
-    , HH.text "private:\n"
-    , HH.text "public:\n"
-    , from [ "\t", name, "();\n" ]
-    , HH.text "}"
-    , from [ "\n\n#endif ", "// ", macroname, "\n" ]
-    ]
-    where
-        baseclass = if isInstant then "InstantCommand" else "Command"
-        macroname = name
-            # Re.split (Re.unsafeRegex "(?=[A-Z])" Re.global)
-            # joinWith "_" # Str.toUpper
-            # ("_" <> _) # (_ <> "_H")
-
 generateSource :: forall p. State -> Element p
-generateSource state@{ description, name } = HH.pre_ $
+generateSource state@{ description, name, fieldPrefix, hasSourceAnn } = HH.pre_ $
     [ commentline description
-    , method "" name [] []
-    , method "void" "Initialize" [] []
-    , method "void" "Execute" [] (texts $ split (Pattern "\n") state.executeBody)
-    , method "bool" "IsFinished" [] []
-    , method "void" "Done" [] []
-    , method "void" "Interrupted" [] []
+    , HH.div_ $ pure $ from ["data ", name, " = ", name]
+    , HH.div_ $ HH.div_ <<< pure <<< HH.text <$> record allFields
     ]
     where
-    method type_ mname args body = HH.span_
-        [ from
-            [ type_
-            , if type_ == "" then "" else " "
-            , name
-            , "::"
-            , mname
-            , "("
-            ]
-        , separate (HH.text ", ") args
-        , from [ ") {\n\t" ]
-        , separate (HH.text "\n\t") body
-        , from [ "\n}\n\n" ]
-        ]
+    allFields = if hasSourceAnn then [fieldPrefix <> "SourceAnn :: SourceAnn"] else []
+    record [] = ["  {}"]
+    record [f] = ["  { " <> f <> " }"]
+    record fs = fs
